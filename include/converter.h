9b593a569d3ee6333a35805140af9d3b6daf01e4
LLynn <lymlym2006@163.com>
2026-03-25 19:52:37 +0800

    Initial commit: NFA to DFA converter(excutable)

---
+++ include/converter.h
@@ -0,0 +1,17 @@
+﻿/**FileHeader
+ * @Author: LLynn
+ * @Date: 2026/3/24 22:52:21
+ * @LastEditors: LLynn
+ * @LastEditTime: 2026/3/25 16:32:09
+ * @Description:
+ * @Copyright: Copyright (©)}) 2026 LLynn. All rights reserved.
+ * @Email: linyumian51@gmail.com
+ */
+#ifndef CONVERTER_H
+#define CONVERTER_H
+#include "automata.h"
+#include "dynamic_enum.h"
+#include <queue>
+StateID_sset get_epsilon_closure(const StateID_sset &StateIDes, const NFA &nfa);
+DFA convert_NFA_to_DFA(NFA nfa);
+#endif
---
+++ include/interaction.h
@@ -0,0 +1,22 @@
+﻿/**FileHeader
+ * @Author: LLynn
+ * @Date: 2026/3/24 22:52:13
+ * @LastEditors: LLynn
+ * @LastEditTime: 2026/3/25 17:04:48
+ * @Description:
+ * @Copyright: Copyright (©)}) 2026 LLynn. All rights reserved.
+ * @Email: linyumian51@gmail.com
+ */
+#ifndef INTERACTION_H
+#define INTERACTION_H
+#include "automata.h"
+#include "dynamic_enum.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <sstream>
+typedef bool StateIDUS;
+StateIDUS read_NFA_from_terminal(NFA &nfa);
+std::string set_to_string(const StateID_sset &s, const NFA &nfa);
+void write_DFA_to_terminal(const DFA &dfa);
+#endif
